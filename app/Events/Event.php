e115a340d05f01f8aa4ed93dd3663d69f692e00c
Edison <15br14ck>
2015-08-10 21:10:44 -0300

    Primeiro commit

---
+++ app/Events/Event.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace jmix\Events;
+
+abstract class Event
+{
+    //
+}
---
+++ app/Http/Kernel.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace jmix\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    /**
+     * The application's global HTTP middleware stack.
+     *
+     * @var array
+     */
+    protected $middleware = [
+        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        \jmix\Http\Middleware\EncryptCookies::class,
+        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+        \Illuminate\Session\Middleware\StartSession::class,
+        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        \jmix\Http\Middleware\VerifyCsrfToken::class,
+    ];
+
+    /**
+     * The application's route middleware.
+     *
+     * @var array
+     */
+    protected $routeMiddleware = [
+        'auth' => \jmix\Http\Middleware\Authenticate::class,
+        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'guest' => \jmix\Http\Middleware\RedirectIfAuthenticated::class,
+    ];
+}
